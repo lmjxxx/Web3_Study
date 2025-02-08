669230e204ca9bad29e51bac7568775fef5e7e28
lmjxxx <limjx20606@kakao.com>
2025-02-08 13:57:34 +0000

    add FIND_NODE request for init

--- kademlia_protocol/src/main.py
+++ kademlia_protocol/src/main.py
@@ -9,6 +9,7 @@ async def main():
     parser = argparse.ArgumentParser(description="Kademlia P2P node")
     parser.add_argument("--port", type=int, help="Port number for the node")
     parser.add_argument("--message", type=str, help="Protocol type PING, FIND_NODE, STORE, FIND_VALUE")
+    parser.add_argument("--ip", type=str, help="IP", default="127.0.0.1")
     parser.add_argument("--bootstrap-node", type=str, help="Bootstrap Node", default="0x000000000000000000")
     args = parser.parse_args()
 
@@ -21,6 +22,22 @@ async def main():
             local_addr=(bootstrap_node.ip, bootstrap_node.port)
         )
         protocol.connection_mode(transport)
+
+    elif args.port and args.message:
+        # new node
+        # bootstrap_node에 FIND_NODE 메시지를 보내서 가까운 노드 목록을 반환 받아 자신의 k-bucket 에 추가하는 것 구현
+        new_node = node.Node(hashlib.sha1(f"{ip}{port}"), args.ip.encode(), args.port)
+        transport, protocol = await loop.create_datagram_endpoint(
+            lambda: kademlia.KademliaProtocol(new_node),
+            local_addr=(new_node.ip, args.port)
+        )
+
+        find_node_request = {
+            "type": "FIND_NODE",
+            "sendor_id": new_node.node_id,
+            "target_id": "0x000000000000000000" # bootstrap_node
+        }
+        new_node.transport.sendto(json.dumps(find_node_request).encode(), (new_node.ip, new_node.port))
 
     try:
         await asyncio.sleep(10)
